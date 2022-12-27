33bd25a65b23092188faf5156f28626618fd1fc8
fallen <fxck@google.com>
2022-12-27 15:58:49 +0300

    added send zip file to email

--- site/main.py
+++ site/main.py
@@ -1,5 +1,12 @@
+import os
 import time
+from zipfile import ZipFile
+import email, smtplib, ssl
 
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from flask import Flask, session, redirect, url_for, escape, request, render_template, flash
 import asyncio
 from secrets import choice
@@ -8,14 +15,13 @@ import sys
 import logging
 from flask_socketio import SocketIO, emit
 import threading
-
-from flask import request, jsonify, copy_current_request_context
-
+from flask import jsonify, copy_current_request_context
+from secret import sender, password
 logging.basicConfig(filename="log.txt", level=logging.DEBUG, format="%(asctime)s %(message)s")
 sys.path.append("/root/fbfarmpro-tg-bot-1.0")
 
-from utils.database import UsersDB, ProductsDB, payment, get_crypto_currency, Tokens
-
+from utils.database import UsersDB, ProductsDB, payment, get_crypto_currency, Tokens, create_random_filename_zip
+from config import MIN_MONEY_PER_BUY
 
 tokens = Tokens("../DB/tokens.db")
 users = UsersDB("site", "../DB/users.db")
@@ -40,8 +46,45 @@ socketio = SocketIO(app)
 name_space = '/abcd'
 
 
+def send_file(file, receiver_email):
+    subject = "Your order"
+    body = "Here is your purchase"
+    message = MIMEMultipart()
+    message["From"] = sender
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message["Bcc"] = receiver_email  # Recommended for mass emails
 
+    # Add body to email
+    message.attach(MIMEText(body, "plain"))
 
+    filename = f"../DB/bought/{file}"  # In same directory as script
+
+    # Open PDF file in binary mode
+    with open(filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    # Encode file in ASCII characters to send by email
+    encoders.encode_base64(part)
+
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
+
+    # Add attachment to message and convert message to string
+    message.attach(part)
+    text = message.as_string()
+
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender, password)
+        server.sendmail(sender, receiver_email, text)
 
 
 @app.route("/")
@@ -75,11 +118,9 @@ def profile():
     else:
         return redirect(url_for("loginpage"))
 
-@app.route("/order<name>")
-def order(name):
-    count = products.get_count_of_products(name)
-    cost = products.get_category_price(name)
-    return render_template("index.html", sost=7, logined=1 if 'userLogged' in session else 0, cost=cost, max=count, name=name)
+@app.route("/order")
+def order():
+    return render_template("index.html", sost=7, logined=1 if 'userLogged' in session else 0)
 
 @app.route("/shop")
 def shop():
@@ -139,11 +180,6 @@ def auth():
             flash("User is not exists!", "error")
             return redirect(url_for("loginpage"))
 
-@app.route("/test")
-def test():
-    products.create_category("name", "desc", 2 )
-    return redirect(url_for("shop"))
-
 @app.route("/logout")
 def logout():
     session.pop("userLogged", None)
@@ -189,7 +225,14 @@ def wait():
                 time.sleep(0.5)
 
     threading.Thread(target=check_token).start()
-    return render_template("index.html", sost=60, logined = 1 if 'userLogged' in session else 0)
+    return render_template("index.html", sost=9, logined = 1 if 'userLogged' in session else 0)
+
+@app.route("/pay<name>")
+def shopp(name):
+    count = products.get_count_of_products(name)
+    cost = products.get_category_price(name)
+    return render_template("index.html", sost=8, logined=1 if 'userLogged' in session else 0, cost=cost, max=count, name=name)
+
 
 @app.route("/telegram")
 def tg_login():
@@ -199,15 +242,44 @@ def tg_login():
     return f"<script>window.open('https://t.me/fbfarmprobot?start={session['token']}', '_blank'); window.open('/waitingtg'); window.close();</script>"
 
 
+@app.route("/addmee")
+def addd():
+    users.add_balance(email=session['email'], amount=1000)
+    return redirect(url_for("profile"))
 
+@app.route("/payment", methods= ['POST'])
+def buy():
+    if request.method == 'POST':
+        category_name = request.form['name']
+        balance = users.get_balance(email=session['email'])
+        cost = int(float(request.form['price'])) * int(float(request.form['amount']))
+        if cost <= balance:
+            users.add_balance(-(cost), email=session['email'])
+            zip_filename = create_random_filename_zip()
+            zip_path = os.path.join("..", "DB", "bought", zip_filename)
+            zipObj = ZipFile(zip_path, "w")
+            for file in products.get_N_products(category_name, int(float(request.form['amount']))):
+                path = os.path.join("..", "DB", category_name, file[0])
+                zipObj.write(path, os.path.basename(path))
+                products.set_isBought(file[0], category_name)
+            zipObj.close()
+            send_file(zip_filename, session['email'])
+            flash("Product(s) was(were) sended to your email!", "error")
+            users.add_purchase(category_name, int(float(request.form['amount'])), int(float(request.form['price'])) * int(float(request.form['amount'])), zip_filename, email=session['email'])
+            return redirect(url_for('profile'))
 @app.route("/buy", methods= ['POST'])
 async def pay():
     if request.method == "POST":
-        print(request.form)
         currency = request.form['currency']
-        amount = (int(float(request.form['price']))*int(float(request.form['amount']))) / await get_crypto_currency(currency)
-        x = await payment.create_payment(amount, "btc".upper())
-        return x
+        amount = int(float(request.form['amount']))
+        if currency in MIN_MONEY_PER_BUY and amount >= MIN_MONEY_PER_BUY[currency]:
+            amount = int(float(request.form['amount'])) / await get_crypto_currency(currency)
+
+            x = await payment.create_payment(amount, currency.upper())
+            return f"<script>window.open('{x['result']['redirectUrl']}', '_blank'); window.open('/profile'); window.close();</script>"
+        else:
+            flash(f'Minimum amount for {currency} is {MIN_MONEY_PER_BUY[currency]}$', 'error')
+            return redirect(url_for("order"))
 
 async def check_for_payments():
     while True:
