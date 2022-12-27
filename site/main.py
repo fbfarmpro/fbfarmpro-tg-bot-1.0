a9d1b7359df04f1988cc24a05f2d1007a042466b
fallen <fxck@google.com>
2022-12-27 19:44:55 +0300

    tg login.. again..

--- site/main.py
+++ site/main.py
@@ -99,15 +99,14 @@ async def check_token():
                 data = users.get_by_id(id)
 
                 userID = data[1]
-                purchases = filter(lambda t: str(t[0]) == str(userID), users.get_purchases()) or None
-                purchase_history = f"\n\n".join( f"Date: {t[2]}\nCategory: {t[3].split('|')[-1]}\nAmount: {t[4]}\nPrice: {t[5]}" for t in purchases)
+                #purchases = filter(lambda t: str(t[0]) == str(userID), users.get_purchases()) or None
+                #purchase_history = f"\n\n".join( f"Date: {t[2]}\nCategory: {t[3].split('|')[-1]}\nAmount: {t[4]}\nPrice: {t[5]}" for t in purchases)
                 user = {
                     'id': userID,
                     'balance': data[5],
-                    'payment_ids': data[6],
-                    'purchase_history': purchases
+                    'payment_ids': data[6]
+                    #'purchase_history': purchases
                 }
-                print(purchases)
                 session['method'] = 'tg'
                 session['user'] = user
                 session['userLogged'] = True
