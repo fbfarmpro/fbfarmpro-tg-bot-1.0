90d2589d673083567cbcf4fbc2be9465278c13e6
root <root@hqnl0260161.online-vm.com>
2022-12-28 15:52:30 +0000

    changed secret_key

--- site/main.py
+++ site/main.py
@@ -43,9 +43,9 @@ def create_random_token():
 
 
 app = Flask(__name__)
-app.secret_key = 'hhhkhkhkkh'
+app.secret_key = 'nnnnnnnnnnnkjlkj'
 # app.config['SECRET_KEY'] = 'D20fndvfMK27^313787-AQl131'
-app.config['SECRET_KEY'] = 'lksdjflaskjdhkjshg'
+app.config['SECRET_KEY'] = '80h908h908ijh0ij'
 
 
 socketio = SocketIO(app)
