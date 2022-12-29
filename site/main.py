eb2ea2e0913734a982211129ce1ce79d6c4cda9d
Kindane <null>
2022-12-29 11:13:16 +0200

    changed

--- site/main.py
+++ site/main.py
@@ -228,7 +228,7 @@ def sendpass():
 	if users.is_registered(email=email):
 		token = create_random_token()
 		tokens.add_email(token=token, email=email)
-		msg = MIMEText(f'Your link for change: <a href="{request.base_url}/change{token}">abc</a>','html')
+		msg = MIMEText(f'Your link for change: <a href="{request.url_root}/change{token}">Change</a>','html')
 		msg['Subject'] = 'subject'
 		msg['From'] = 'fb-farm.pro'
 		msg['To'] = email 
