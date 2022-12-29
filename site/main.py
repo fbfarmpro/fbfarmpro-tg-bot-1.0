35750c9043518d1e2af4acb694398c2e943c81e9
fallen <fxck@google.com>
2022-12-29 20:44:21 +0300

    final

--- site/main.py
+++ site/main.py
@@ -441,7 +441,7 @@ async def check_for_payments():
 
 
 if __name__ == '__main__':
-	from waitress import serve
-	serve(app, host="0.0.0.0", port=5000)
+	#from waitress import serve
+	socketio.run(app, host='0.0.0.0')
 
 
