6a682643deeca0c185dd502031d1b69a7bf5151b
Kindane <null>
2023-01-16 20:02:26 +0200

    trying to fix filecmp

--- bot.py
+++ bot.py
@@ -246,58 +246,41 @@ async def change_advertisement():
                 next_theme = random.choice(themes)
                 # TODO fix cmp files
                 for filename in config.AD_FILES:
+                    """
                     if filecmp.cmp(os.path.join(config.AD_FOLDER, next_theme, filename),
                                    os.path.join(config.AD_IMG_FOLDER, filename)) != 0:
+                    """
 
+                    file1 = open(os.path.join(config.AD_FOLDER, next_theme, filename), "rb")
+                    file2 = open(os.path.join(config.AD_IMG_FOLDER, filename), "rb")
+                    if file1.read() != file2.read():  # cmp 2 files
                         need_to_copy = open(os.path.join(config.AD_FOLDER, next_theme, filename), "rb")
                         where_to_copy = open(os.path.join(config.AD_IMG_FOLDER, filename), "wb")
                         where_to_copy.write(need_to_copy.read())
                         need_to_copy.close()
                         where_to_copy.close()
-                        """
-                        path1 = os.path.join(config.AD_FOLDER, next_theme, filename)
-                        path2 = os.path.join(config.AD_IMG_FOLDER, filename)
-                        shutil.copyfile(path1,
-                                        path2)
-                        print(path1, path2)
-                        print("found", next_theme)
-                        """
                         ok = True
+                    file1.close()
+                    file2.close()
                 if ok:
                     print("copy config")
-                    """
-                    shutil.copyfile(os.path.join(config.AD_FOLDER, next_theme, config.AD_TEXT_FILENAME),
-                                    os.path.join(config.AD_CURRENT_FOLDER, config.AD_TEXT_FILENAME))
-                    """
                     with open(os.path.join(config.AD_FOLDER, next_theme, config.AD_TEXT_FILENAME), "r") as file:
                         with open(os.path.join(config.AD_CURRENT_FOLDER, config.AD_TEXT_FILENAME), "w") as file2:
                             file2.write(file.read())
 
-                """
-                if len(filecmp.dircmp(os.path.join(config.AD_FOLDER, next_theme), config.AD_CURRENT_FOLDER).diff_files) != 0:
-                    ok = True  # ok, i found new theme
-                    # now copy files from that theme to 'current' folder
-                    for filename in config.AD_FILES:
-                        # copy from next_theme to current
-                        shutil.copy(os.path.join(config.AD_FOLDER, next_theme, filename),
-                                    os.path.join(config.AD_FOLDER, "current", filename))
-                        # and copy from current to img folder
-                        shutil.copy(os.path.join(config.AD_FOLDER, next_theme, filename),
-                                    os.path.join(config.AD_FOLDER, "current", filename))
-                """
             await asyncio.sleep(random.randint(10, 60))  # sleep 'till next change
         else:
             # if there are only default theme and current theme is not default -> change theme
             ok = True
             for filename in config.AD_FILES:
                 print("cmp0")
+                """
                 if not filecmp.cmp(os.path.join(config.AD_DEFAULT_FOLDER, filename),
                                    os.path.join(config.AD_IMG_FOLDER, filename)):
-                    print("cmpSUCCESS")
-                    """
-                    shutil.copyfile(os.path.join(config.AD_DEFAULT_FOLDER, filename),
-                                    os.path.join(config.AD_IMG_FOLDER, filename))
-                    """
+               """
+                file1 = open(os.path.join(config.AD_DEFAULT_FOLDER, filename), "rb")
+                file2 = open(os.path.join(config.AD_IMG_FOLDER, filename), "rb")
+                if file1.read() != file2.read():  # cmp 2 files
                     need_to_copy = open(os.path.join(config.AD_DEFAULT_FOLDER, filename), "rb")
                     where_to_copy = open(os.path.join(config.AD_IMG_FOLDER, filename), "wb")
                     where_to_copy.write(need_to_copy.read())
